48a3d82a8dae74194914f476baeeab31fa3b3e89
Ian Gramovich <gramovich@disroot.org>
2021-03-30 00:07:11 +0300

    Added third party library and support doxygen

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,10 +1,12 @@
 #include <spline/integral.hpp> 
 #include <spline/diff.hpp>
+#include <tinynurbs/tinynurbs.h>
 #include <iostream>
 
 int main()
 {
     Integral i;
     Diff d;
+    tinynurbs::Curve<float> crv;
     return 0;
 }
